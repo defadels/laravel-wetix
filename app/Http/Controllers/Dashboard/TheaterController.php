277c0a8673a5b80be5914f7dbbddfb21c18e0ef8
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-06-08 21:55:12 +0700

    make controller and model for theater

---
+++ app/Http/Controllers/Dashboard/TheaterController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Theater;
+use Illuminate\Http\Request;
+
+class TheaterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Theater  $theater
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Theater $theater)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Theater  $theater
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Theater $theater)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Theater  $theater
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Theater $theater)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Theater  $theater
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Theater $theater)
+    {
+        //
+    }
+}
