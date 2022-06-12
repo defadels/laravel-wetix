2879b488543b7241b82dc32a4de79721f870cb4b
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-06-12 21:54:40 +0700

    Make theater softdelete

--- app/Models/Theater.php
+++ app/Models/Theater.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Theater extends Model
 {
@@ -10,5 +11,6 @@ class Theater extends Model
 
   protected $guarded = [];
 
+  use SoftDeletes;
 
 }
